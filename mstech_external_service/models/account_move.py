b8495a298f002818151df990ba67a8adb0f50a08
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-05 11:48:52 -0500

    Delete mstech_external_service directory

--- mstech_external_service/models/account_move.py
+++
@@ -1,9 +0,0 @@
-from odoo import models,fields,api,_
-from datetime import timedelta, datetime
-from odoo.exceptions import UserError
-
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-    
-    jasatec_os= fields.Char(string='O/S')
-    jasatec_constancia= fields.Char(string='Constancia')
