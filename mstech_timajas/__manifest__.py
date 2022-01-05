3827b761a437923e9e2d8a5afbf20aaa9080934c
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-05 11:52:33 -0500

    Add files via upload

---
+++ mstech_timajas/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Mstech Timajas",
+    "category": "External Service",
+    'author': "Meditech",
+    'summary':"Mstech External Service - Timajas",
+    'depends': [
+        'sale',
+        'stock',
+        'industry_fsm',
+        'industry_fsm_sale',
+
+    ],
+    'data': [
+        'data/project_data.xml',
+        'views/project_views.xml',
+        'views/view_project_task.xml',
+    ],
+    'installable' : True,
+    'auto_install' :  False,
+    'application' :  False,
+}
