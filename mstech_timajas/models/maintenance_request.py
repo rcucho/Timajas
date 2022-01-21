5457a14d3b2f598f550dead67d045f802cb63882
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-21 12:31:16 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -6,7 +6,18 @@ class MaintenanceRequestTimajas(models.Model):
     _inherit = "maintenance.request"
     
     mant_project = fields.Many2one('project.task',string="Proyecto")
-
+    
+    @api.onchange('name')
+    def _compute_mant_project(self):
+        for record in self:
+            if record.name:
+                record.mant_project = self.env['project.task'].create({
+                    'name': record.name,
+                    'user_ids' : record.employee_id,
+                    'project_id' : 3,
+                })
+    
+    
 class ProjectTaskTimajas(models.Model):
     _inherit = "project.task"
     
