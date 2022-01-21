02f89b99d9e3cffdd9704c9139aae70ecb966c6e
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-21 15:18:59 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 class MaintenanceRequestTimajas(models.Model):
     _inherit = "maintenance.request"
     
-    mant_project = fields.Many2one('project.task',string="Proyecto", readonly=True)
+    mant_project = fields.Many2one('project.task', string="Proyecto")
     
     @api.onchange('name')
     def _compute_mant_project(self):
