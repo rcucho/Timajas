0ebbaf7e4e1dd491062d66a75eb15177395b0f3f
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-21 18:28:49 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -19,14 +19,20 @@ class MaintenanceRequestTimajas(models.Model):
 class MaintenanceEquipment2(models.Model):
     _inherit = "maintenance.equipment"
     
+    eqip_product = fields.Many2one('product.template', string="Producto")
+    
     @api.model
     def create(self, vals):
         for record in self:
-            product = self.env['product.template'].create({
+            eqip_product = self.env['product.template'].create({
                 'name': record.name
             })
         equipment = super(MaintenanceEquipment2, self).create(vals)
         return equipment
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+    
+    product_eqip = fields.One2many('maintenance.equipment', 'eqip_product', string="Equipamento de Mantenimiento")
     
 class ProjectTaskTimajas(models.Model):
     _inherit = "project.task"
