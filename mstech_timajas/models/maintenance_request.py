2e4a4a6c4bd74d902aedbd9f8f7f33c9fc80ee85
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-21 15:27:29 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -17,7 +17,8 @@ class MaintenanceRequestTimajas(models.Model):
                     'user_ids' : [record.employee_id.id,],
                     'project_id' : 3,
                 })
-                raise UserError("Esta mal el %s",record.employee_id.id)
+                raise UserError(str(record.employee_id.id))
+                raise UserError(_("..."))
     
     
 class ProjectTaskTimajas(models.Model):
