7ced87a72f5606520ae5116651ddf33de380925e
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-24 09:58:51 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -44,12 +44,12 @@ class ProductTemplate(models.Model):
     _inherit = "product.product"
     
     product_eqip = fields.One2many('maintenance.equipment', 'eqip_product', string="Equipamento de Mantenimiento")
-    project_count = fields.Integer(compute='_compute_project_count', string="Project Count", store=True)
+    project_count = fields.Integer(compute='_compute_project_count', string="Project Count", store= False)
     
     @api.depends('stock_move_ids')
     def _compute_project_count(self):
         for record in self:
-            record.project_count = len(record.stock_move_ids.filtered(lambda x: x.picking_id.picking_task != False))
+            record.project_count = len(record.stock_move_ids.filtered(lambda x: x.picking_id.picking_task))
     
 class ProductTemplate(models.Model):
     _inherit = "product.template"
