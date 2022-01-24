9afa3ddbe6912b8dc9bd0a28b4b5eb7da8e9c349
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-24 18:10:23 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -55,9 +55,10 @@ class ProductTemplate(models.Model):
         for record in self:
             stock = self.env['stock.move'].search([('product_id','=',record.id)])
             pick = stock.picking_id
-            move_pro = pick.mapped('move_ids_without_package')
-            quant_pro = move_pro.quantity_done
-            record.project_count = quant_pro
+            move_pro = pick.move_ids_without_package
+            for m in move_pro:
+                quant_pro = quant_pro + m.quantity_done
+                record.project_count = quant_pro
             #task = pick.picking_task
             #record.project_count = len(task)
            
