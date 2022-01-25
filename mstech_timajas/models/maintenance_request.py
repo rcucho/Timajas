8f4ee35c058f8240dffbe4e70040262f3bf6c348
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-25 08:59:02 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -26,14 +26,14 @@ class MaintenanceEquipment2(models.Model):
     #eqip_task = fields.Many2one('project.task', string="Equipo en Tareas")#, compute='_compute_eqip_task')
     eqip_task = fields.One2many('project.task','task_eqip', string="Equipo en Tareas")
     #-----------------------------------------------------------------------------------------
-    '''@api.model
+    @api.model
     def create(self, vals):
         equipment = super(MaintenanceEquipment2, self).create(vals)
         for record in equipment:
             record.eqip_product = self.env['product.product'].create({
                 'name': record.name
             })
-        return equipment'''
+        return equipment
     
     @api.onchange('maintenance_ids')
     def _compute_eqip_task(self):
