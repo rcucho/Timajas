c81f601f393f2263fb559177eec2bbf15840341e
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-25 09:56:24 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -63,7 +63,7 @@ class ProductTemplate(models.Model):
             #pick = stock.picking_id.search([('picking_task.project_id','=',record.project_pids[0].project_id.id)])
             #pick = stock.picking_id.search([('picking_task','=',record.project_pids[0])])
             #pick = stock.picking_id.search([('picking_task.task_eqip','=',record.project_pids.task_eqip)])
-            pick = stock.picking_id.search([('picking_task.task_eqip.id','=',record.product_eqip[0].id])
+            pick = stock.picking_id.search([('picking_task.task_eqip.id','=',record.product_eqip[0].id)])
             move_pro = pick.move_ids_without_package
             for m in move_pro:
                 qnt_pro = qnt_pro + m.quantity_done
