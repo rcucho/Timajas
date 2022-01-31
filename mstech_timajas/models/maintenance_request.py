61c44be81fcc38c5cbc82da22190903cb45aef7d
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-31 13:05:42 -0500

    Update maintenance_request.py

--- mstech_timajas/models/maintenance_request.py
+++ mstech_timajas/models/maintenance_request.py
@@ -68,11 +68,11 @@ class MaintenanceEquipment2(models.Model):
     def _compute_mant_lote(self):
         for rec in self:
             #if rec.eqip_product:
-            #if rec.serial_no:
-            rec.mant_lote = self.env['stock.production.lot'].create({
-                'name': rec.serial_no,
-                'company_id' : rec.company_id.id,
-                'product_id' : rec.eqip_product.id,
+            if rec.serial_no:
+                rec.mant_lote = self.env['stock.production.lot'].create({
+                    'name': rec.serial_no,
+                    'company_id' : rec.company_id.id,
+                    'product_id' : rec.eqip_product.id,
             })
                 
     #------------------------------------------------------------------------------------------
